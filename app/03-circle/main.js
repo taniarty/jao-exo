ffe8390151a0701bb8e7bd801564eb5c5ab73c3c
Tatiana PETIPRE <tatiana.petipre@ineat-conseil.fr>
2018-06-05 08:51:43 +0100

    add svgNS

--- app/03-circle/main.js
+++ app/03-circle/main.js
@@ -1,21 +1,37 @@
 // document.querySelector = $ de jQuery
 
 class Circle {
-    constructor(){}
+    constructor(radius){
+        this.radius = radius;
+    }
 
     draw(){
         const div = document.querySelector('div');
+        const r = this.radius;
         div.innerHTML = `            
-    <svg height="600" width="600">
-        <circle cx="300" cy="300" r="200" stroke="black" stroke-width="2" fill="transparent" />
+    <svg height="${3*r}" width="${3*r}">
+        <circle cx="${1.5*r}" cy="${1.5*r}" r="${r}" stroke="black" stroke-width="2" fill="transparent" />
     </svg>
         `;
+    }
+    setPoint(angle) {
+        const svgns = "http://www.w3.org/2000/svg";
+        const svg = document.querySelector('svg');
+        const point = document.createElementNS(svgns, 'circle');
+        point.setAttribute('cx', 1.5 * this.radius);
+        point.setAttribute('cy', 1.5 * this.radius);
+        point.setAttribute('r', 10);
+        point.setAttribute('fill', '#000');
+        point.setAttribute('stroke', 'yellow');
+        point.setAttribute('stroke-width', 2);
+        svg.appendChild(point);
     }
 }
 
 function main(){
-    const c = new Circle();
+    const c = new Circle(200);
     c.draw();
+    c.setPoint(10);
 }
 
 main();
