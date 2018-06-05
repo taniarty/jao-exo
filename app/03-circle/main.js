7748643d41f36610a50db7ce7e94722c421cfe23
Tatiana PETIPRE <tatiana.petipre@ineat-conseil.fr>
2018-06-05 15:32:51 +0100

    ok

--- app/03-circle/main.js
+++ app/03-circle/main.js
@@ -1,9 +1,28 @@
-function main(){
+//Sync
+
+/*function main(){
     for (let i = 0; i < 200; i++){
         setTimeout(() => {
             const m = new Multiply(i, 3);
             m.draw();
         }, 16 * i);
+    }
+}*/
+
+//Async
+function sleep(delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, delay);
+    });
+}
+
+async function main(){
+    for (let i = 0; i < 30; i++){
+        const m = new Multiply(i, 2);
+        m.draw();
+        await sleep(16);
     }
 }
 
