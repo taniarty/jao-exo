35db20d0a9175b82da9f78c413ff363c4779b515
Tatiana PETIPRE <tatiana.petipre@ineat-conseil.fr>
2018-06-05 09:08:36 +0100

    add points

--- app/03-circle/main.js
+++ app/03-circle/main.js
@@ -3,6 +3,8 @@
 class Circle {
     constructor(radius){
         this.radius = radius;
+        this.cx = 1.5 * radius;
+        this.cy = 1.5 * radius;
     }
 
     draw(){
@@ -15,14 +17,15 @@ class Circle {
         `;
     }
     setPoint(angle) {
+        const a = - angle * 2 * Math.PI / 360;
         const svgns = "http://www.w3.org/2000/svg";
         const svg = document.querySelector('svg');
         const point = document.createElementNS(svgns, 'circle');
-        point.setAttribute('cx', 1.5 * this.radius);
-        point.setAttribute('cy', 1.5 * this.radius);
-        point.setAttribute('r', 10);
+        point.setAttribute('cx',this.cx + this.radius * Math.cos(a)); //horizontal
+        point.setAttribute('cy', this.cy + this.radius * Math.sin(a)); // verticale
+        point.setAttribute('r', 3);
         point.setAttribute('fill', '#000');
-        point.setAttribute('stroke', 'yellow');
+        point.setAttribute('stroke', '#000');
         point.setAttribute('stroke-width', 2);
         svg.appendChild(point);
     }
@@ -31,7 +34,12 @@ class Circle {
 function main(){
     const c = new Circle(200);
     c.draw();
-    c.setPoint(10);
+    c.setPoint(0);
+    c.setPoint(30);
+    c.setPoint(60);
+    c.setPoint(90);
+    c.setPoint(180);
+    c.setPoint(270);
 }
 
 main();
