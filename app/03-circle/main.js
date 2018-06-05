b336b7d3ededb60ae356aecc5e269f721bc88fda
Tatiana PETIPRE <tatiana.petipre@ineat-conseil.fr>
2018-06-05 08:30:07 +0100

    creation class

--- app/03-circle/main.js
+++ app/03-circle/main.js
@@ -1,12 +1,21 @@
 // document.querySelector = $ de jQuery
 
-function makeCircle(){
-    const div = document.querySelector('div');
-    div.innerHTML = `            
+class Circle {
+    constructor(){}
+
+    draw(){
+        const div = document.querySelector('div');
+        div.innerHTML = `            
     <svg height="600" width="600">
         <circle cx="300" cy="300" r="200" stroke="black" stroke-width="2" fill="transparent" />
     </svg>
-    `;
+        `;
+    }
 }
 
-makeCircle();
+function main(){
+    const c = new Circle();
+    c.draw();
+}
+
+main();
