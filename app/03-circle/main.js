25ef35eadd8196511cff70aad2d19fd2f939e3b5
Tatiana PETIPRE <tatiana.petipre@ineat-conseil.fr>
2018-06-05 08:22:36 +0100

    function makeCircle

--- app/03-circle/main.js
+++ app/03-circle/main.js
@@ -1,1 +1,12 @@
+// document.querySelector = $ de jQuery
 
+function makeCircle(){
+    const div = document.querySelector('div');
+    div.innerHTML = `            
+    <svg height="600" width="600">
+        <circle cx="300" cy="300" r="200" stroke="black" stroke-width="2" fill="transparent" />
+    </svg>
+    `;
+}
+
+makeCircle();
