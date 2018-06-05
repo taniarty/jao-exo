0443ca2332810ca708dda46e69c45ede381798c8
Tatiana PETIPRE <tatiana.petipre@ineat-conseil.fr>
2018-06-05 09:28:48 +0100

    add line

--- app/03-circle/main.js
+++ app/03-circle/main.js
@@ -1,5 +1,7 @@
 // document.querySelector = $ de jQuery
 
+const SVGNS = "http://www.w3.org/2000/svg";
+
 class Circle {
     constructor(radius){
         this.radius = radius;
@@ -16,18 +18,32 @@ class Circle {
     </svg>
         `;
     }
+
     setPoint(angle) {
         const a = - angle * 2 * Math.PI / 360;
-        const svgns = "http://www.w3.org/2000/svg";
         const svg = document.querySelector('svg');
-        const point = document.createElementNS(svgns, 'circle');
+        const point = document.createElementNS(SVGNS, 'circle');
         point.setAttribute('cx',this.cx + this.radius * Math.cos(a)); //horizontal
-        point.setAttribute('cy', this.cy + this.radius * Math.sin(a)); // verticale
+        point.setAttribute('cy', this.cy + this.radius * Math.sin(a)); // vertical
         point.setAttribute('r', 3);
         point.setAttribute('fill', '#000');
         point.setAttribute('stroke', '#000');
         point.setAttribute('stroke-width', 2);
         svg.appendChild(point);
+    }
+
+    setLine(angle1, angle2){
+        const a1 = - angle1 * 2 * Math.PI / 360;
+        const a2 = - angle2 * 2 * Math.PI / 360;
+        const svg = document.querySelector('svg');
+        const line = document.createElementNS(SVGNS, 'line');
+        line.setAttribute('x1', this.cx + this.radius * Math.cos(a1)); //horizontal
+        line.setAttribute('y1', this.cy + this.radius * Math.sin(a1)); // vertical
+        line.setAttribute('x2', this.cx + this.radius * Math.cos(a2));
+        line.setAttribute('y2', this.cy + this.radius * Math.sin(a2));
+        line.setAttribute('stroke', '#000');
+        line.setAttribute('stroke-width', 2);
+        svg.appendChild(line);
     }
 }
 
@@ -40,6 +56,7 @@ function main(){
     c.setPoint(90);
     c.setPoint(180);
     c.setPoint(270);
+    c.setLine(30,90);
 }
 
 main();
